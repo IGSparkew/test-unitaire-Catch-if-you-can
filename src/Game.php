2d54bef79f5fa6e8ca7fe340694676606213cacc
IGSparkew <40170910+IGSparkew@users.noreply.github.com>
2023-07-25 15:28:03 +0200

    test hadTowPlayer

--- src/Game.php
+++ src/Game.php
@@ -1,5 +1,16 @@
 <?php 
     class Game {
 
+        public function __construct(private Player $playerFirst, private Player $playerSecond) {
+        }
+
+        public function getFirstPlayer(): Player {
+            return $this->playerFirst;
+        }
+
+        public function getSecondPlayer() : Player {
+            return $this->playerSecond;
+        }
+
     }
 ?>
--- test/GameTest.php
+++ test/GameTest.php
@@ -1,15 +1,23 @@
 <?php
 include "./src/Game.php";
+include "./src/Player.php";
+
 use PHPUnit\Framework\TestCase;
 
+use function PHPUnit\Framework\assertNotNull;
 use function PHPUnit\Framework\assertTrue;
 
     class GameTest extends TestCase {
     
-
         public static function testHadGame() {
-            $game = new Game();
+            $game = new Game(new Player(), new Player());
             assertTrue($game instanceof Game);
+        }
+
+        public static function testGameHadtowPlayers() {
+            $game = new Game(new Player(), new Player());
+            assertNotNull($game->getFirstPlayer());
+            assertNotNull($game->getSecondPlayer());
         }
 
 
