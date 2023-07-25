80e5fdd0f39e31eb459dfc78c0de75dbdfef2ea5
IGSparkew <40170910+IGSparkew@users.noreply.github.com>
2023-07-25 15:36:16 +0200

    feat: ajout de la grille + test de ça taille

--- src/Game.php
+++ src/Game.php
@@ -1,7 +1,15 @@
 <?php 
     class Game {
 
+        private $grid;
+
         public function __construct(private Player $playerFirst, private Player $playerSecond) {
+            $this->grid = [];
+
+            for ($i = 0; $i < 10; $i++) {
+                $col = [0,0,0,0,0,0,0,0,0,0];
+                array_push($this->grid, $col);
+            }
         }
 
         public function getFirstPlayer(): Player {
@@ -10,6 +18,10 @@
 
         public function getSecondPlayer() : Player {
             return $this->playerSecond;
+        }
+
+        public function getGrid(): Array {
+            return $this->grid;
         }
 
     }
--- test/GameTest.php
+++ test/GameTest.php
@@ -4,6 +4,7 @@ include "./src/Player.php";
 
 use PHPUnit\Framework\TestCase;
 
+use function PHPUnit\Framework\assertEquals;
 use function PHPUnit\Framework\assertNotNull;
 use function PHPUnit\Framework\assertTrue;
 
@@ -18,6 +19,14 @@ use function PHPUnit\Framework\assertTrue;
             $game = new Game(new Player(), new Player());
             assertNotNull($game->getFirstPlayer());
             assertNotNull($game->getSecondPlayer());
+        }
+
+        public static function testGameHadGrid() {
+            $game = new Game(new Player(), new Player());
+            assertEquals(10, count($game->getGrid()));
+            foreach($game->getGrid() as $row) {
+                assertEquals(10, count($row));
+            }
         }
 
 
