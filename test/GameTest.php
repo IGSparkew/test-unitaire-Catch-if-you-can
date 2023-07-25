13be96e5ec842589bf44eee72e454753633f53b9
IGSparkew <40170910+IGSparkew@users.noreply.github.com>
2023-07-25 15:17:49 +0200

    feat: ajout de la class Game

--- test/GameTest.php
+++ test/GameTest.php
@@ -1,11 +1,11 @@
 <?php
-
+include "./src/Game.php";
 use PHPUnit\Framework\TestCase;
 
-use function PHPUnit\Framework\assertNotNull;
 use function PHPUnit\Framework\assertTrue;
 
     class GameTest extends TestCase {
+    
 
         public static function testHadGame() {
             $game = new Game();
